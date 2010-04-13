365b21f1a44d953a64beb8bbf134cd49f85256b7
hpoydar <hpoydar@gmail.com>
2010-04-13 17:05:01 -0400

    Attempt to autoload macros only if installed as a plugin

--- lib/shoulda/rails.rb
+++ lib/shoulda/rails.rb
@@ -7,11 +7,9 @@ require 'shoulda/action_controller' if defined? ActionController::Base
 require 'shoulda/action_view'       if defined? ActionView::Base
 require 'shoulda/action_mailer'     if defined? ActionMailer::Base
 
-root = if defined?(Rails.root) && Rails.root
-         Rails.root
-       else
-         RAILS_ROOT
-       end
-
-# load in the 3rd party macros from vendorized plugins and gems
-Shoulda.autoload_macros(root, File.join("vendor", "{plugins,gems}", "*")) unless root.nil?
+# Load in the 3rd party macros from vendorized plugins and gems
+# Only works if installed as a plugin, since as a gem Rails.root
+# is not initialized yet
+if defined?(Rails.root) && Rails.root
+  Shoulda.autoload_macros root, File.join("vendor", "{plugins,gems}", "*")
+end
